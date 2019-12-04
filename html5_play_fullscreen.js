83f5a3eb99b778d8152eb3144acf458fd16609c1
Zzqqbs <23348661+Zzqqbs@users.noreply.github.com>
2019-12-04 13:05:31 +0800

    Update html5_play_fullscreen.js
    修复引入接口

--- html5_play_fullscreen.js
+++ html5_play_fullscreen.js
@@ -5,10 +5,12 @@
 // @description AcFun 播放器自动播放下一集时，保持当前窗口全屏状态
 // @icon        https://cdn.aixifan.com/ico/favicon.ico
 // @include     /^https?:\/\/www.acfun.cn/bangumi/\w+(\d+_){2}\d+$/
+// @grant       GM_setValue
+// @grant       GM_getValue
 // ==/UserScript==
 (function () {
     let loopTime = 20;
-    
+
     function getTip(a) {
         return a.getElementsByClassName('tip-fullscreen')[0];
     }
@@ -21,7 +23,7 @@
 
     function fullscreen_read() {
         GM_setValue('web', fullscreen_web_tip.innerText);
-        GM_setValue('screen', fullscreen_screen_tip.innerText)
+        GM_setValue('screen', fullscreen_screen_tip.innerText);
     }
 
     function fullscreen_write() {
